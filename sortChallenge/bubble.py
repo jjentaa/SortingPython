c3fa4159e05061f479326fd196da04663dc9de12
_MadNess <86233076+jjentaa@users.noreply.github.com>
2021-12-07 15:04:13 +0700

    Update bubble.py

--- sortChallenge/bubble.py
+++ sortChallenge/bubble.py
@@ -1,7 +1,7 @@
 def bbs(arr):
     for i in range(len(arr)):
-        for j in range(i):
-            if arr[i] < arr[j]:
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
     
     return arr
